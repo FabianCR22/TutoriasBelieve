a3f23e64d4289bdeeb55e651d8968f78c952eea1
fabia <fabia@LAPTOP-6G1B96NL>
2022-03-07 19:17:43 -0600

    Se crea el proyecto Tutorias Believe

---
+++ src/main/java/com/TutoriasBelieve/TutoriasBelieveApplication.java
@@ -0,0 +1,13 @@
+package com.TutoriasBelieve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoriasBelieveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoriasBelieveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TutoriasBelieve/TutoriasBelieveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TutoriasBelieve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoriasBelieveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
